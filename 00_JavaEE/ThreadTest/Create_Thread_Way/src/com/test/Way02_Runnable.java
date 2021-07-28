95a288c6143cf3312d0b24b2ef79023e77faee63
luofeng21944 <feng.luo@hand-china.com>
2021-07-28 16:00:21 +0800

    提交

---
+++ 00_JavaEE/ThreadTest/Create_Thread_Way/src/com/test/Way02_Runnable.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class Way02_Runnable implements Runnable {
+    public static void main(String[] args) {
+        Way02_Runnable way02 = new Way02_Runnable();
+        new Thread(way02).start();
+
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(Thread.currentThread().getName()+" "+i);
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(Thread.currentThread().getName()+" "+i);
+        }
+    }
+}
