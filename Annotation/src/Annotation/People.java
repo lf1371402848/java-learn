618540cedbeec19b0101cfa868e2c8cfc4fc61e8
lf <13607063440@163.com>
2021-07-31 16:20:06 +0800

    jjk

---
+++ Annotation/src/Annotation/People.java
@@ -0,0 +1,10 @@
+package Annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface People {
+    Game[] value() ;
+}
---
+++ Annotation/src/TestAnno.java
@@ -0,0 +1,13 @@
+import Annotation.Game;
+
+/**
+ * @Description: TODO
+ * @Author: lf
+ * @Date: 2021/7/3 21:31
+ * @Version: 1.0
+ */
+
+@Game(value = "元素1")
+@Game(value = "元素2")
+public class TestAnno {
+}
